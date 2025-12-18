4e26edac88318f3d65bef8b4ba9aa73e44613f03
utigernils <utigernils@gmail.com>
2025-12-18 23:18:06 +0100

    refactor; add a function to play a turn (i hope that's alright ;) )

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
 import { Board, Player } from "./board.ts";
 
-const board = new Board();
-let player = Player.PlayerX;
-
-board.output();
-
-while (true) {
-  const input = prompt(`Player ${player}:`) || "";
+export function playTurn(board: Board, player: Player, input: string): { row: number, winner: Player } {
   const col = Number.parseInt(input);
   const row = board.makeMove(player, col);
-  board.output();
-  console.log();
   const winner = board.winner(player, row, col);
-  if (winner != Player.Nobody) {
-    console.log(`Player ${player}: A winner is you!`);
-    break;
+  return { row, winner };
+}
+
+if (import.meta.main) {
+  const board = new Board();
+  let player = Player.PlayerX;
+
+  board.output();
+
+  while (true) {
+    const input = prompt(`Player ${player}:`) || "";
+    const { row, winner } = playTurn(board, player, input);
+    board.output();
+    console.log();
+    if (winner != Player.Nobody) {
+      console.log(`Player ${player}: A winner is you!`);
+      break;
+    }
+    player = player == Player.PlayerX ? Player.PlayerO : Player.PlayerX;
   }
-  player = player == Player.PlayerX ? Player.PlayerO : Player.PlayerX;
 }
